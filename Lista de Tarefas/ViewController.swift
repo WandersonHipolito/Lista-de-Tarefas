22962057dc5ca9361c70fb7d65edf0e1ab1b53ed
Wanderson Hipolito <wandersonhipolito1989@gmail.com>
2020-09-03 15:06:09 -0300

    Initial Commit

---
+++ Lista de Tarefas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lista de Tarefas
+//
+//  Created by Wanderson Hipolito on 03/09/20.
+//  Copyright © 2020 Wanderson Hipolito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
